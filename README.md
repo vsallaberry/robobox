201523874b583c6ff02ceb156d210c3ba85e3a1b
vincent <vsallaberry@gmail.com>
2017-12-31 23:21:15 +0100

    typo, README, LICENSE

---
+++ README.md
@@ -0,0 +1,26 @@
+
+## robobox
+----------
+
+* [Overview](#overview)
+* [System Requirments](#systemrequirments)
+* [Details](#details)
+* [Contact](#contact)
+* [License](#license)
+
+## Overview
+**robobox** is a repository regroupping dev about robobox [www.robobox.fr].
+
+## System requirements
+- Arduino [www.arduino.cc].
+
+## Details
+- n/a
+
+## Contact
+[vsallaberry@gmail.com]
+<https://github.com/vsallaberry/robobox>
+
+## License
+GPLv3 or later. See LICENSE file.
+
--- m01/m01_v1/m01_v1.ino
+++ m01/m01_v1/m01_v1.ino
@@ -1,4 +1,22 @@
 // vim syntax:cpp
+/*
+ * Copyright (C) 2017 Vincent Sallaberry
+ * vrobobox <https://github.com/vsallaberry/robobox>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 // BOARD / PIN definitions.
 #define PIN_LED     5   // Définition du 'pin' de sortie de la LED
 #define PIN_PROBE   2   // Définition du 'pin' du capteur de mouvement
